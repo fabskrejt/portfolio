ce2e5be262e4bab2ccd14120ccc5eecdd4cde954
Vladimir Shits <rav9aa@gmail.com>
2022-01-08 00:48:36 +0300

    refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./header/Header";
-import {About} from "./about/About";
+import {Main} from "./main/Main";
 import {Skills} from "./skills/Skills";
 import {Projects} from "./projekkts/Projects";
 
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
             <Header/>
-            <About/>
+            <Main/>
             <Skills/>
             <Projects/>
         </div>
---
+++ src/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Main.module.css'
+
+export const Main = () => {
+    return (
+        <div className={styles.mainBlock}>
+            <div className={styles.container}>
+                <div className={styles.textAboutNe}>
+                    Привет! Меня Зовут и я прихожу.
+                </div>
+                <img src={''}/>
+            </div>
+        </div>
+    )
+}
