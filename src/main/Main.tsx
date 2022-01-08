eb327defbfa97be05db3ba5aab8b9656fd38742f
Vladimir Shits <rav9aa@gmail.com>
2022-01-08 18:00:20 +0300

    added comoon styles

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './Main.module.css'
+import stylesContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
         <div className={styles.mainBlock}>
-            <div className={styles.container}>
+            <div className={stylesContainer.container}>
                 <div className={styles.text}>
                 <span>Hi There</span>
                     <h1>I am Vladimir Sits</h1>
