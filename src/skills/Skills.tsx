ad210bbd5ac6050151e0684d75f30a6375bcd91c
Vladimir Shits <rav9aa@gmail.com>
2022-03-20 21:48:00 +0300

    added icons for skills

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from './Skills.module.css'
 import {Skill} from "./skill/Skill";
 import stylesContainer from '../common/styles/Container.module.css'
+import {faReact} from "@fortawesome/free-brands-svg-icons/faReact";
+import {faJs} from "@fortawesome/free-brands-svg-icons/faJs";
+import {faCss3} from "@fortawesome/free-brands-svg-icons/faCss3";
 
 export const Skills = () => {
     return (
@@ -9,9 +12,9 @@ export const Skills = () => {
             <div className={`${styles.container} ${stylesContainer.container}`}>
                 <h2 className={styles.title}>My Skills</h2>
                 <div className={styles.skills}>
-                    <Skill title={'CSS'} description={'Som text for test'}/>
-                    <Skill title={'React'} description={'Som text for test. Just my skill, one of a million'}/>
-                    <Skill title={'JS'} description={'Som text for test. I know it. It is easy for me if you don\'t ask about it' }/>
+                    <Skill title={'JS'} description={'Som text for test. I know it. It is easy for me if you don\'t ask about it' } icon={faJs}/>
+                    <Skill title={'React'} description={'Som text for test. Just my skill, one of a million'} icon={faReact}/>
+                    <Skill title={'CSS'} description={'Som text for test'} icon={faCss3}/>
                 </div>
             </div>
 
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from './Skill.module.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faReact} from "@fortawesome/free-brands-svg-icons/faReact";
+
 
 type SkillPropsType = {
     title: string
     description: string
+    icon:any
 }
 
 export const Skill = (props:SkillPropsType) => {
 return(
     <div className={styles.skill}>
-        <div className={styles.icon}></div>
+      {/*  <div className={styles.icon}></div>*/}
+        <FontAwesomeIcon icon={props.icon} size={'5x'} color={'#fec544'}/>
         <h3>{props.title}</h3>
+
         <span className={styles.description}>{props.description}</span>
     </div>
 )
