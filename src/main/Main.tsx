807edb01a8b55d322511cca3d590ba73676ec088
Vladimir Shits <rav9aa@gmail.com>
2022-03-23 23:56:01 +0300

    added photo for main

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './Main.module.css'
 import stylesContainer from '../common/styles/Container.module.css'
-
+import photo from '../assets/img/mainPhoto.jpg'
 export const Main = () => {
     return (
         <div className={styles.mainBlock}>
@@ -12,7 +12,7 @@ export const Main = () => {
                     <span>I am a frontend web developer</span>
                 </div>
                 <div className={styles.img}>
-                    <img src={''}/>
+                    <img src={photo}/>
                 </div>
             </div>
         </div>
