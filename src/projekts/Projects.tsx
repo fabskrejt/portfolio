cb88a1b9f476b47797c9b1996453d3886c75ca0d
Vladimir Shits <rav9aa@gmail.com>
2022-03-24 00:30:09 +0300

    added links to projects

--- src/projekts/Projects.tsx
+++ src/projekts/Projects.tsx
@@ -9,9 +9,12 @@ export const Projects = () => {
             <div className={`${stylesContainer.container} ${styles.container}`}>
                 <h2 className={styles.title}>My Projects</h2>
                 <div className={styles.projects}>
-                    <Project title={'Todolist'} description={'One of my projects'}/>
-                    <Project title={'Social Network'} description={'One of my projects'}/>
-                    <Project title={'Cards'} description={'One of my projects'}/>
+                    <Project title={'Todolist'} description={'One of my projects'}
+                             refToProject={'https://fabskrejt.github.io/todolist-wed/'}/>
+                    <Project title={'Social Network'} description={'One of my projects'}
+                             refToProject={"https://fabskrejt.github.io/social_network_react_ts_project/"}/>
+                    <Project title={'Cards'} description={'One of my projects'}
+                             refToProject={'https://fabskrejt.github.io/card-learning/'}/>
                 </div>
             </div>
 
--- src/projekts/project/Project.tsx
+++ src/projekts/project/Project.tsx
@@ -4,12 +4,13 @@ import styles from './Project.module.css'
 type ProjectPropsType ={
     title: string
     description:string
+    refToProject:string
 }
 export const Project =(props: ProjectPropsType)=>{
     return(
         <div className={styles.project}>
                 <div className={styles.previewImg}>
-                    <a href={''}>Watch</a>
+                    <a target={'_blank'} href={props.refToProject} >Watch</a>
                 </div>
             <h3 className={styles.title}>{props.title}</h3>
             <span className={styles.description}>{props.description}</span>
