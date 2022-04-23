7ce75bb03c29e7ee013f1b722a5fa4b71dbe7cea
Vladimir Shits <rav9aa@gmail.com>
2022-04-23 23:50:17 +0300

    added tsparticles

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -2,9 +2,51 @@ import React from "react";
 import styles from './Main.module.css'
 import stylesContainer from '../common/styles/Container.module.css'
 import photo from '../assets/img/mainPhoto.jpg'
+import Particles from "react-tsparticles";
+import {loadFull} from "tsparticles";
+
+
+const particlesOpt ={
+    fpsLimit: 60,
+    "fullScreen": {
+        "enable": false,
+        "zIndex": 1
+    },
+    particles: {
+        color: {
+            value: "#1e2c65"
+        },
+        links: {
+            enable: true,
+            color: "#00ffa6",
+            distance: 130,
+        },
+        move: {
+            enable: true
+        }
+    }
+}
+
 export const Main = () => {
+    const particlesInit = async (main: any) => {
+        await loadFull(main);
+    };
+
+    const particlesLoaded = () => {
+        return Promise.resolve()
+    };
+
     return (
         <div className={styles.mainBlock}>
+            <Particles
+                canvasClassName={styles.particlesCanvas}
+                className={styles.particles}
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={particlesOpt}
+                width='100%'
+                height='100vh'
+            />
             <div className={stylesContainer.container}>
                 <div className={styles.text}>
                     <span>Hi There</span>
@@ -12,7 +54,7 @@ export const Main = () => {
                     <span>I am a frontend web developer</span>
                 </div>
                 <div className={styles.img}>
-                    <img src={photo}/>
+                    <img src={photo} alt={'ownerPhoto'}/>
                 </div>
             </div>
         </div>
