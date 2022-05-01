b22bad91282280c6caecfb7e7891405a5e3c65b6
Vladimir Shits <rav9aa@gmail.com>
2022-05-01 22:19:50 +0300

    added sass for project

--- src/projekts/project/Project.tsx
+++ src/projekts/project/Project.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import styles from './Project.module.css'
+import styles from './Project.module.scss'
 
-type ProjectPropsType ={
+type ProjectPropsType = {
     title: string
-    description:string
-    refToProject:string
+    description: string
+    refToProject: string
     style: {}
 }
-export const Project =(props: ProjectPropsType)=>{
+export const Project = (props: ProjectPropsType) => {
 
-    return(
+    return (
         <div className={styles.project}>
-                <div className={styles.previewImg} style={props.style}>
-                    <a target={'_blank'} href={props.refToProject} >Watch</a>
-                </div>
+            <div className={styles.previewImg} style={props.style}>
+                <a target={'_blank'} href={props.refToProject}>Watch</a>
+            </div>
             <h3 className={styles.title}>{props.title}</h3>
             <span className={styles.description}>{props.description}</span>
         </div>
