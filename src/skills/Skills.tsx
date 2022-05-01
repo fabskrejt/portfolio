8716e81648728eabc31611c556f221bcaad479c3
Vladimir Shits <rav9aa@gmail.com>
2022-05-01 22:31:46 +0300

    added sass for skill

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './Skills.module.css'
+import styles from './Skills.module.scss'
 import {Skill} from "./skill/Skill";
 import stylesContainer from '../common/styles/Container.module.css'
 import {faReact} from "@fortawesome/free-brands-svg-icons/faReact";
@@ -18,13 +18,21 @@ export const Skills = () => {
                     <Skill title={'JS'}
                            description={'I have a good knowledge of javascript and I continue to improve it.'}
                            icon={faJs}/>
-                    <Skill title={'TypeScript'} description={'I use Typescript to type my projects, which simplifies the development process.'}  icon={''} localIcon={typescript}/>
+                    <Skill title={'TypeScript'}
+                           description={'I use Typescript to type my projects, which simplifies the development process.'}
+                           icon={''} localIcon={typescript}/>
                     <Skill title={'React'} description={'I have experience in creating SPA applications.' +
                     ' I like using React/redux, but that doesn\'t mean I don\'t want to try other tools.'}
                            icon={faReact}/>
-                    <Skill title={'Redux'} description={'In my projects, I use redux, redux toolkit to manage the state.'} icon={''} localIcon={redux}/>
-                    <Skill title={'HTML'} description={'I use it in my projects. I understand what BEM is, what semantics is and why it is important.'} icon={faHtml5}/>
-                    <Skill title={'CSS'} description={'I use this in every project. I use Grid, Flex, media queries and others.'} icon={faCss3}/>
+                    <Skill title={'Redux'}
+                           description={'In my projects, I use redux, redux toolkit to manage the state.'} icon={''}
+                           localIcon={redux}/>
+                    <Skill title={'HTML'}
+                           description={'I use it in my projects. I understand what BEM is, what semantics is and why it is important.'}
+                           icon={faHtml5}/>
+                    <Skill title={'CSS'}
+                           description={'I use this in every project. I use Grid, Flex, media queries and others.'}
+                           icon={faCss3}/>
                 </div>
             </div>
 
